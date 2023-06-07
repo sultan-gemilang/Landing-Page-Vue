31b8c170380c68dc33845c55b60663478e273101
sultan-gemilang <sultangemilang24@gmail.com>
2023-06-07 09:16:43 +0700

    yey selesai :D
    lottie documentation for vue sucks

--- Landing-Page-Vue/src/main.js
+++ Landing-Page-Vue/src/main.js
@@ -1,4 +1,10 @@
+import "bootstrap/dist/css/bootstrap.css"
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import "@lottiefiles/lottie-player"
 
 createApp(App).mount('#app')
+// App.use(LottieVuePlayer)
+
+import "bootstrap/dist/js/bootstrap.js"
---
+++ landing-page-vue/src/components/HeroMain.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="hero">
+      <div class="container-fluid">
+          <div class="row flex-lg-row-reverse align-items-center g-5 py-5 px-3">
+            <div class="col-10 col-sm-8 col-lg-6">
+              <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_sk5h1kfn.json" background="transparent" speed="1" style="width: 100%; height: auto;" loop autoplay></lottie-player>
+            </div>
+            <div class="col-lg-6">
+              <h1 class="display-5 fw-bold lh-1 mb-3">Di kacangin Oleh Co-Worker Anda?</h1>
+              <p class="lead" style="font-size: 24px;">Peneliti mengatakan bahwah 50% dari tempat kerja yang memiliki sistem Co-worker akan mengalami depresi sebab di kacangin selama kerja. Ungkapkan pemikiran mu dan tegak kan free speech bersama dengan Elon Musk!</p>
+              <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
